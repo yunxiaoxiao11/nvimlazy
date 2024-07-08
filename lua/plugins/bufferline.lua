f33085ad0ccaae4fb89b66096b682dd48dd9803c
root <root@xiaoyu>
2024-07-08 22:21:52 +0800

    格式化修改

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,16 +1,16 @@
 return {
-    'akinsho/bufferline.nvim',
-    event = "VeryLazy",
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        local bufferline = require('bufferline')
-        bufferline.setup {
-            options = {
-                mode = 'buffers',
-                numbers='ordinal',
-                middle_mouse_command='bdelete! %d'
-            }
-        }
-    end
+  'akinsho/bufferline.nvim',
+  event = "VeryLazy",
+  version = "*",
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    local bufferline = require('bufferline')
+    bufferline.setup {
+      options = {
+        mode = 'buffers',
+        numbers = 'ordinal',
+        middle_mouse_command = 'bdelete! %d'
+      }
+    }
+  end
 }
--- lua/plugins/mini.surround.lua
+++ lua/plugins/mini.surround.lua
@@ -1,33 +1,33 @@
 return {
-    "echasnovski/mini.surround",
-    event = "VeryLazy",
-    recommended = true,
-    keys = function(_, keys)
-        -- Populate the keys based on the user's options
-        local opts = LazyVim.opts("mini.surround")
-        local mappings = {
-            { opts.mappings.add,            desc = "Add Surrounding",                     mode = { "n", "v" } },
-            { opts.mappings.delete,         desc = "Delete Surrounding" },
-            { opts.mappings.find,           desc = "Find Right Surrounding" },
-            { opts.mappings.find_left,      desc = "Find Left Surrounding" },
-            { opts.mappings.highlight,      desc = "Highlight Surrounding" },
-            { opts.mappings.replace,        desc = "Replace Surrounding" },
-            { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
-        }
-        mappings = vim.tbl_filter(function(m)
-            return m[1] and #m[1] > 0
-        end, mappings)
-        return vim.list_extend(mappings, keys)
-    end,
-    opts = {
-        mappings = {
-            add = "ys",      -- Add surrounding in Normal and Visual modes
-            delete = "ds",   -- Delete surrounding
-            replace = "cs",  -- Replace surrounding
-            find = "gf",     -- Find surrounding (to the right)
-            find_left = "gF", -- Find surrounding (to the left)
-            highlight = "gh", -- Highlight surrounding
-            update_n_lines = "gn", -- Update `n_lines`
-        },
+  "echasnovski/mini.surround",
+  event = "VeryLazy",
+  recommended = true,
+  keys = function(_, keys)
+    -- Populate the keys based on the user's options
+    local opts = LazyVim.opts("mini.surround")
+    local mappings = {
+      { opts.mappings.add,            desc = "Add Surrounding",                     mode = { "n", "v" } },
+      { opts.mappings.delete,         desc = "Delete Surrounding" },
+      { opts.mappings.find,           desc = "Find Right Surrounding" },
+      { opts.mappings.find_left,      desc = "Find Left Surrounding" },
+      { opts.mappings.highlight,      desc = "Highlight Surrounding" },
+      { opts.mappings.replace,        desc = "Replace Surrounding" },
+      { opts.mappings.update_n_lines, desc = "Update `MiniSurround.config.n_lines`" },
+    }
+    mappings = vim.tbl_filter(function(m)
+      return m[1] and #m[1] > 0
+    end, mappings)
+    return vim.list_extend(mappings, keys)
+  end,
+  opts = {
+    mappings = {
+      add = "ys",            -- Add surrounding in Normal and Visual modes
+      delete = "ds",         -- Delete surrounding
+      replace = "cs",        -- Replace surrounding
+      find = "gf",           -- Find surrounding (to the right)
+      find_left = "gF",      -- Find surrounding (to the left)
+      highlight = "gh",      -- Highlight surrounding
+      update_n_lines = "gn", -- Update `n_lines`
     },
+  },
 }
